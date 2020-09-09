5faf350363a8fb940ef68e66821323357a6afe50
javim7 <61723252+javim7@users.noreply.github.com>
2020-09-09 15:40:29 -0600

    Update almacen.java
    agregue otro metodo para agregar la informacion de las empresas en la lista. Actualice tambien el metodo de iniciar sesion.

--- almacen.java
+++ almacen.java
@@ -11,21 +11,33 @@ public class almacen {
 
     public boolean registrar_usuario(usuario nombre, usuario contraseña) {
 
-        //agregando la informacion a las listas
+        //agregando la informacion del usuario a la lista de usuario
         usuario.add(nombre);
         usuario.add(contraseña);
 
         return true;
     }
 
-    public boolean iniciar_sesion(String nombre, String contraseña){
+    
+    public boolean registrar_empresa(empresa nombre, empresa contraseña) {
+
+        //agregando la informacion de la empresa a la lista de empresa
+        empresa.add(nombre);
+        empresa.add(contraseña);
+
+        return true;
+    }
+
+    public boolean iniciar_sesion(String nombre, String contraseña){ //meotod para ver si la informacion de iniciar sesion ya esta en las listas
 
         if(usuario.contains(nombre) && usuario.contains(contraseña)) {
+            return true;
+        } else if(empresa.contains(nombre) && empresa.contains(contraseña)) {
             return true;
         } else {
             return false;
         }
-    }
+    }    
 
 
 }
