925cb05391c2495a2e78e46105799d51affa1663
javim7 <61723252+javim7@users.noreply.github.com>
2020-09-09 15:28:57 -0600

    Almacen
    metodo para agregar la informacion de registros a la lista y metodo para ver si el inicio de sesion esta en la lista.

--- almacen.java
+++ almacen.java
@@ -1,23 +1,31 @@
 import java.util.ArrayList;
 public class almacen {
 
-    private ArrayList<usuario> empresa;
-    private ArrayList<empresa> empresario;
+    private ArrayList<usuario> usuario;
+    private ArrayList<empresa> empresa;
 
     public almacen(){
+        usuario = new ArrayList();
         empresa = new ArrayList();
-        empresario = new ArrayList();
+    }
+
+    public boolean registrar_usuario(usuario nombre, usuario contraseña) {
+
+        //agregando la informacion a las listas
+        usuario.add(nombre);
+        usuario.add(contraseña);
+
+        return true;
     }
 
     public boolean iniciar_sesion(String nombre, String contraseña){
 
-        if(nombre == contraseña) {
+        if(usuario.contains(nombre) && usuario.contains(contraseña)) {
             return true;
         } else {
             return false;
         }
     }
-
 
 
 }
